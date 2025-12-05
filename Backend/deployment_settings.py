fd6882069ca8911e3e1b37e7e0c3a3df209dcb3d
takatsy <zafisolomasy@gmail.com>
2025-12-05 13:56:50 +0300

    my new commit

--- Backend/deployment_settings.py
+++ Backend/deployment_settings.py
@@ -9,3 +9,36 @@ CSRF_TRUSTED_ORIGINS = ['https://'+os.environ.get('RENDER_EXTERNAL_HOSTNAME')]
 DEBUG = False
 SECRET_KEY = os.environ.get['SECRET_KEY']
 
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+]
+
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000'
+# ]
+
+STORAGES ={
+    "default":{
+        "BACKEND" : "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles" : {
+        "BACKEND" : "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default= os.environ['DATABASE_URL'],
+        conn_max_age=600
+    )
+}
+
